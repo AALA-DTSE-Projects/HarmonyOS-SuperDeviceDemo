d013299aa0147e144b4c7605859e71ee5ff53717
Tran Thanh Huy 00564191 <tran.thanh.huy@huawei.com>
2021-02-26 17:19:01 +0900

    Init project

---
+++ tablet/src/main/java/com/huawei/superdevicedemo/MyApplication.java
@@ -0,0 +1,10 @@
+package com.huawei.superdevicedemo;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
